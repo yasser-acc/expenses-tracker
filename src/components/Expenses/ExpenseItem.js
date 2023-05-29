2c4672d9b2d8291e5c8bc0b37514a30cf755cf71
yasser-acc <yassirmastour1600@gmail.com>
2023-05-29 12:54:39 +0100

    Added the first click event listener using react state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  //get access to special variables and change their data through events
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("updated !!!!!");
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>Change Title</button>
       </div>
     </Card>
   );
