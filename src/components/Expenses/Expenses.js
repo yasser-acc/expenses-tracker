31ad86eda698bf4606205a57319d78ebf0b8391d
yasser-acc <yassirmastour1600@gmail.com>
2023-06-01 12:34:22 +0100

    Added useState() to expenses.js

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
   const filterChangeHandler = (selectedYear) => {
-    console.log("expenses.js");
-    console.log(selectedYear);
+    setFilteredYear(selectedYear);
   };
 
   return (
