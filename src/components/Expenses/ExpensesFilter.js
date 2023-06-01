c4a32fb735f1bcecc5e5d9dd6edf0cf64004605d
yasser-acc <yassirmastour1600@gmail.com>
2023-06-01 11:29:39 +0100

    Updated the Expenses Filter Component

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -12,6 +12,8 @@ const ExpensesFilter = () => {
       <div className="expenses-filter__control">
         <label>Filter by year</label>
         <select onChange={dropdownChangeHandler}>
+          <option value="2022">2024</option>
+          <option value="2022">2023</option>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -60,7 +60,7 @@ const ExpenseForm = (props) => {
           <label>Date</label>
           <input
             type="date"
-            min="2020-01-01"
+            min="2019-01-01"
             max="2024-12-31"
             onChange={DateChangeHandler}
             value={enteredDate}
