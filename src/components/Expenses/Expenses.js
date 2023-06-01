eb3743e988b9d1c292f86c6c2bdd155ea59bb127
yasser-acc <yassirmastour1600@gmail.com>
2023-06-01 11:43:57 +0100

    Update ExpensesFilter.js

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,10 +5,15 @@ import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 function Expenses(props) {
+  const filterChangeHandler = (selectedYear) => {
+    console.log("expenses.js");
+    console.log(selectedYear);
+  };
+
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter />
+        <ExpensesFilter onChangeFilter={filterChangeHandler} />
         <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
