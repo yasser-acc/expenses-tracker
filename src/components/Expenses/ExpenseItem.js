84943f1f83b61b9c2b074fe4e20315291637a1ee
yasser-acc <yassirmastour1600@gmail.com>
2023-06-02 12:00:20 +0100

    ExpenseItem.js Updated
    Changed the expense item item to be stateless

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  //get access to special variables and change their data through events
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("updated !!!!!");
-    console.log(title);
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
-        <button onClick={clickHandler}>Change Title</button>
       </div>
     </Card>
   );
