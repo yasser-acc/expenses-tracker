6fd14b0072464f9651c3a87648287bd5d962b13d
yasser-acc <yassirmastour1600@gmail.com>
2023-06-05 13:09:52 +0100

    removed hard coded array items and used map() function instead
    To render dynamic list of data and get rid of hard coded components I used map() to get the data I want to render directly from the first array that holds expense Items

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -18,26 +18,19 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {
+          //render an array of every expense element
+          //rendring lists of data
+          //this will help us get rid of hard coded expenseItems we used before
+
+          props.items.map((expense) => (
+            <ExpenseItem
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        }
       </Card>
     </div>
   );
