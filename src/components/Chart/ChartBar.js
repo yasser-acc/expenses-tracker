514c216c7ea6d655d74ccbaa2171598ee8293695
yasser-acc <yassirmastour1600@gmail.com>
2023-06-13 10:12:22 +0100

    Setted up dynamic height for the chart bar fill div

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,6 +1,24 @@
 import React from "react";
 import "./ChartBar.css";
 
-const ChartBar = (props) => {};
+const ChartBar = (props) => {
+  let barFillHeight = "0%";
+  if (props.max) {
+    //setting up the bar fill height for the chart-bar__fill div
+    //barFillHeight will be used as a dynamic css property for this div
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
+  }
+  return (
+    <div className="chart-bar">
+      <div className="chart-bar__inner">
+        <div
+          className="chart-bar__fill"
+          style={{ height: barFillHeight }}
+        ></div>
+      </div>
+      <div className="chart-bar__label">{props.label}</div>
+    </div>
+  );
+};
 
 export default ChartBar;
